c0593558115f8c31e353771b0934c25befd79071
parallel-kevin <601406483@qq.com>
2023-02-04 00:38:44 +0800

    feat: mobile adaption

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,18 @@ import "swiper/css/bundle";
 import { Box } from "@chakra-ui/react";
 import React, { ReactElement } from "react";
 import { Outlet } from "react-router-dom";
+import { isMobile } from "react-device-detect";
 import Header from "./components/Header";
+import { MobileNotification } from "./components/MobileNotification";
 
 const App = ({ children }: React.PropsWithChildren<unknown>): ReactElement => {
+    if (isMobile) {
+        return (
+            <Box minH="100vh" bg="black" color="white">
+                <MobileNotification />
+            </Box>
+        );
+    }
     return (
         // TO-DO: use color mode when implementing light/dark
         <Box minH="100vh" bg="black" color="white">
---
+++ src/components/MobileNotification.tsx
@@ -0,0 +1,46 @@
+import { Container, Box, Heading, Img } from "@chakra-ui/react";
+import React, { ReactElement } from "react";
+
+import banner from "../assets/home-bg.png";
+import Helicopter from "../assets/aviation-9.svg";
+
+export const MobileNotification = (): ReactElement => {
+    const helicopterWidth =
+        window.innerWidth / 2 > 300 ? 300 : window.innerWidth / 2;
+    return (
+        <Container
+            maxW="100%"
+            minH="100vh"
+            bgImg={banner}
+            bgSize="cover"
+            bgPos="top left"
+            bgRepeat="no-repeat"
+            p="0"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+        >
+            <Box
+                width="90vw"
+                maxW="400px"
+                bg="rgba(255, 255, 255, 0.7)"
+                border="3px solid #FFFFFF"
+                borderRadius="20px"
+                padding="24px"
+                pos="relative"
+            >
+                <Heading fontSize="20px" textAlign="center" color="black">
+                    Please access with PC for better experience.
+                </Heading>
+                <Img
+                    src={Helicopter}
+                    pos="absolute"
+                    right={-helicopterWidth / 4.5}
+                    bottom={-helicopterWidth / 2.5}
+                    w={helicopterWidth}
+                    transform="rotate(-24deg)"
+                />
+            </Box>
+        </Container>
+    );
+};
