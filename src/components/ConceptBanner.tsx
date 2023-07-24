da9d57b8564d2cafcd455e5ea2bf8541a3a17037
cj <745960380@qq.com>
2023-07-24 21:32:24 +0800

    fix: home page animation

--- src/components/ConceptBanner.tsx
+++ src/components/ConceptBanner.tsx
@@ -1,6 +1,57 @@
 import { Heading, Image, Box, Text } from "@chakra-ui/react";
 import React, { ReactElement } from "react";
 import Primitives from "@/components/Home/assets/primitive.png";
+import Rectangles from "@/components/Home/assets/rectangles.svg";
+import TextImg2 from "@/components/Home/assets/textImg2.svg";
+import TextImg3 from "@/components/Home/assets/textImg3.svg";
+import Vet from "@/components/Home/assets/vet.svg";
+import Aiming from "@/components/Home/assets/aiming.svg";
+
+const Content = ({ textImg }: { textImg: string }) => {
+    return (
+        <Box
+            sx={{
+                width: "610px",
+                height: "172px",
+                background: `url(${textImg})`,
+                backgroundRepeat: "no-repeat",
+                position: "relative",
+                transition: "all 0.5s ease",
+                "&:hover >div": {
+                    background: "transparent",
+                    backdropFilter: "none",
+                },
+                "&:hover img": {
+                    transform: "rotate(90deg)",
+                },
+            }}
+        >
+            <Box
+                sx={{
+                    width: "calc(100% - 4px)",
+                    height: "calc(100% - 6px)",
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    background: "rgba(253, 225, 185, 0.20)",
+                    backdropFilter: "blur(11px)",
+                    transition: "all 0.5s ease",
+                }}
+            ></Box>
+            <Image
+                src={Aiming}
+                sx={{
+                    position: "absolute",
+                    left: "-100px",
+                    top: "-16px",
+                    transition: "all 0.5s ease",
+                    transform: "rotate(45deg)",
+                }}
+            ></Image>
+        </Box>
+    );
+};
 
 const ConceptBanner = (): ReactElement => {
     return (
@@ -11,15 +62,106 @@ const ConceptBanner = (): ReactElement => {
                 display: "flex",
                 alignItems: "center",
                 justifyContent: "center",
+                fontFamily: "Orbitron",
             }}
         >
-            <Image
-                src={Primitives}
+            <Box
                 sx={{
                     width: "90%",
                     maxWidth: "1500px",
                 }}
-            />
+            >
+                <Box
+                    sx={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        color: "#fde189",
+                        fontSize: "16px",
+                    }}
+                >
+                    <Text>CLASSIFIED TOP SECRET</Text>
+                    <Text>CLASSIFIED TOP SECRET</Text>
+                </Box>
+                <Box sx={{ background: "#FDE1B933" }}>
+                    <Box
+                        sx={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            position: "relative",
+                            borderBottom: "2px solid #fde189",
+                        }}
+                    >
+                        <Image
+                            width="30%"
+                            src={Vet}
+                            sx={{ position: "absolute", left: 0 }}
+                        ></Image>
+                        <Text
+                            sx={{
+                                fontSize: "48px",
+                                color: "#fde189",
+                                fontWeight: "600",
+                            }}
+                        >
+                            Primitives Definition
+                        </Text>
+                        <Image
+                            src={Vet}
+                            width="30%"
+                            sx={{ position: "absolute", right: 0 }}
+                        ></Image>
+                    </Box>
+
+                    <Box
+                        sx={{
+                            borderBottom: "2px solid #fde189",
+                            paddingBottom: "40px",
+                        }}
+                    >
+                        <Box
+                            sx={{
+                                display: "flex",
+                                justifyContent: "center",
+                                marginTop: "60px",
+                            }}
+                        >
+                            <Content textImg={Rectangles}></Content>
+                        </Box>
+                        <Box
+                            sx={{
+                                display: "flex",
+                                justifyContent: "flex-end",
+                                paddingRight: "200px",
+                                marginTop: "60px",
+                            }}
+                        >
+                            <Content textImg={TextImg2}></Content>
+                        </Box>
+
+                        <Box
+                            sx={{
+                                display: "flex",
+                                justifyContent: "flex-end",
+                                paddingRight: "80px",
+                                marginTop: "60px",
+                            }}
+                        >
+                            <Content textImg={TextImg3}></Content>
+                        </Box>
+                    </Box>
+
+                    <Box sx={{ paddingBottom: "26px" }}>
+                        <Box
+                            sx={{
+                                background: `url(${Vet})`,
+                                height: "30px",
+                                marginTop: "16px",
+                            }}
+                        ></Box>
+                    </Box>
+                </Box>
+            </Box>
         </Box>
     );
 };
--- src/components/Home/Game.tsx
+++ src/components/Home/Game.tsx
@@ -1,11 +1,34 @@
 import { Box, Image } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect } from "react";
 import GameTitle from "@/components/Home/assets/game-title.svg";
-import SectionActivities from "@/components/Home/assets/sectionActivities.png";
-import Tournament from "@/components/Home/assets/tournament.png";
+import GrayPlanet from "@/components/Home/assets/gray-planet.svg";
+import SectionActivities from "@/components/Home/assets/gamebg.svg";
+import Tournament from "@/components/Home/assets/tournament.svg";
+import BidTitle from "@/components/Home/assets/bid-title.svg";
+import TrailblazerTitle from "@/components/Home/assets/trailblazer-title.svg";
+import YellowPlanet from "@/components/Home/assets/yellow-planet.svg";
+import YellowishPlanet from "@/components/Home/assets/yellowish-planet.svg";
+import CenterPlanet from "@/components/Home/assets/center-planet.svg";
+import TransparentPlanet from "@/components/Home/assets/transparent-planet.svg";
+
 import { useNavigate } from "react-router-dom";
 const Game = () => {
     const navigate = useNavigate();
+    const [active, setActive] = React.useState(false);
+
+    useEffect(() => {
+        const sections = document.getElementById("games");
+        window.addEventListener("scroll", () => {
+            const currentScrollPos = window.scrollY + sections.offsetHeight;
+            const top = sections.offsetHeight / 2 + sections.offsetTop;
+            const bottom = top + sections.offsetHeight / 2;
+            if (currentScrollPos > top && currentScrollPos < bottom) {
+                setActive(true);
+            } else {
+                setActive(false);
+            }
+        });
+    }, []);
     return (
         <Box
             id="games"
@@ -26,19 +49,146 @@ const Game = () => {
                     width: "1153px",
                     height: "470px",
                     position: "relative",
+                    "& *": {
+                        transition: "all 0.5s ease",
+                    },
+                    "& img": {
+                        position: "absolute",
+                    },
+                    // trailblazer title
+                    "& .trailblazer-title": {
+                        left: "-120px",
+                        top: "82px",
+                    },
+                    ".trailblazer-title-move": {
+                        left: "-80px",
+                        top: "100px",
+                    },
+                    // blue planet
+                    "& .blue-planet": {
+                        left: "-120px",
+                        top: "40px",
+                        cursor: "pointer",
+                    },
+                    ".blue-planet-move": {
+                        left: "-80px",
+                        top: "100px",
+                        transform: "scale(1.4)",
+                    },
+                    // bid title
+                    "& .bid-title": {
+                        right: "0px",
+                        top: "133px",
+                    },
+                    ".bid-title-move": {
+                        right: "100px",
+                        top: "80px",
+                    },
+                    // gray planet
+                    "& .gray-planet": {
+                        right: "110px",
+                        top: "185px",
+                    },
+                    ".gray-planet-move": {
+                        right: "110px",
+                        top: "100px",
+                        transform: "scale(0.8)",
+                    },
+
+                    // center planet
+                    "& .center-planet": {
+                        left: "340px",
+                        top: "28px",
+                    },
+                    ".center-planet-move": {
+                        left: "220px",
+                        top: "28px",
+                    },
+
+                    // transparent planet
+                    "& .transparent-planet": {
+                        left: "225px",
+                        top: "24px",
+                    },
+                    ".transparent-planet-move": {
+                        left: "140px",
+                        top: "80px",
+                    },
+
+                    // yellow planet
+                    "& .yellow-planet": {
+                        right: "390px",
+                        top: "140px",
+                    },
+                    ".yellow-planet-move": {
+                        right: "460px",
+                        top: "100px",
+                    },
+
+                    // yellowish planet
+                    "& .yellowish-planet": {
+                        left: "260px",
+                        bottom: "100px",
+                    },
+                    ".yellowish-planet-move": {
+                        left: "460px",
+                        bottom: "80px",
+                    },
                 }}
             >
+                <Box>
+                    <Image
+                        src={TrailblazerTitle}
+                        className={`trailblazer-title ${
+                            active && "trailblazer-title-move"
+                        }`}
+                    ></Image>
+                    <Image
+                        className={`blue-planet ${
+                            active && "blue-planet-move"
+                        }`}
+                        src={Tournament}
+                        onClick={() => {
+                            navigate("/trailblazer");
+                        }}
+                    ></Image>
+                </Box>
+
+                <Box>
+                    <Image
+                        src={BidTitle}
+                        className={`bid-title ${active && "bid-title-move"}`}
+                    ></Image>
+                    <Image
+                        src={GrayPlanet}
+                        className={`gray-planet ${
+                            active && "gray-planet-move"
+                        }`}
+                    ></Image>
+                </Box>
                 <Image
-                    src={Tournament}
-                    sx={{
-                        position: "absolute",
-                        left: "-100px",
-                        top: "80px",
-                        cursor: "pointer",
-                    }}
-                    onClick={() => {
-                        navigate("/trailblazer");
-                    }}
+                    src={CenterPlanet}
+                    className={`center-planet ${
+                        active && "center-planet-move"
+                    }`}
+                ></Image>
+                <Image
+                    src={TransparentPlanet}
+                    className={`transparent-planet ${
+                        active && "transparent-planet-move"
+                    }}`}
+                ></Image>
+                <Image
+                    src={YellowPlanet}
+                    className={`yellow-planet ${
+                        active && "yellow-planet-move"
+                    }`}
+                ></Image>
+                <Image
+                    src={YellowishPlanet}
+                    className={`yellowish-planet ${
+                        active && "yellowish-planet-mov"
+                    }`}
                 ></Image>
             </Box>
         </Box>
