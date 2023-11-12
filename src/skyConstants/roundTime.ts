41995c048b7d9961700d1aa0fd96b4b8078c4142
cj <745960380@qq.com>
2023-11-12 09:39:21 +0800

    fix: winner

--- src/skyConstants/roundTime.ts
+++ src/skyConstants/roundTime.ts
@@ -9,13 +9,13 @@ const RoundTime: any = {
             {
                 address: "0xaC7605770E89ef96F68A081815b2fb8D59532896",
                 img: images.find((item: any) =>
-                    item.includes("/static/media/1."),
+                    item.includes("/static/media/8."),
                 ),
             },
             {
                 address: "0xD3E401814d1FaA8ca0419EccCBFeE93aC7B15B31",
                 img: images.find((item: any) =>
-                    item.includes("/static/media/2."),
+                    item.includes("/static/media/9."),
                 ),
             },
         ],
